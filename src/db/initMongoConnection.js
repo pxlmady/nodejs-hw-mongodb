2e9c1c256504b5f13de4cfeba831717147f25f6c
pxlmady <yoplufre@gmail.com>
2024-06-01 11:17:30 +0300

    add db

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const initMongoConnection = async () => {
-  const mongoUri = `mongodb+srv://yoplufre:jd1khY0BwPSHQcTF@cluster0.viitrda.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  const mongoUri = `mongodb+srv://yoplufre:jd1khY0BwPSHQcTF@cluster0.viitrda.mongodb.net/contacts?retryWrites=true&w=majority&appName=Cluster0`;
   try {
     await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
